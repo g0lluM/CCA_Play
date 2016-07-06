43638cc00548ec9a5c29601df21cc99d4daee029
Antonin TESSIER <antonintessier@gmail.com>
2016-07-06 10:57:51 +0100

    First shot

---
+++ target/scala-2.11/twirl/main/views/html/main.template.scala
@@ -0,0 +1,53 @@
+
+package views.html
+
+import play.twirl.api._
+import play.twirl.api.TemplateMagic._
+
+import play.api.templates.PlayMagic._
+import models._
+import controllers._
+import play.api.i18n._
+import play.api.mvc._
+import play.api.data._
+import views.html._
+
+/**/
+object main extends BaseScalaTemplate[play.twirl.api.HtmlFormat.Appendable,Format[play.twirl.api.HtmlFormat.Appendable]](play.twirl.api.HtmlFormat) with play.twirl.api.Template2[String,Html,play.twirl.api.HtmlFormat.Appendable] {
+
+  /**/
+  def apply/*1.2*/(title: String)(content: Html):play.twirl.api.HtmlFormat.Appendable = {
+      _display_ {
+
+Seq[Any](format.raw/*1.32*/("""
+
+"""),format.raw/*3.1*/("""<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>"""),_display_(/*7.17*/title),format.raw/*7.22*/("""</title>
+    </head>
+    <body>
+        <h1>It works!</h1>
+    </body>
+</html>
+"""))}
+  }
+
+  def render(title:String,content:Html): play.twirl.api.HtmlFormat.Appendable = apply(title)(content)
+
+  def f:((String) => (Html) => play.twirl.api.HtmlFormat.Appendable) = (title) => (content) => apply(title)(content)
+
+  def ref: this.type = this
+
+}
+              /*
+                  -- GENERATED --
+                  DATE: Wed Jul 06 10:55:14 BST 2016
+                  SOURCE: /home/antonin/IdeaProjects/HelloWorld/app/views/main.scala.html
+                  HASH: e714d1f012632da5844cc565fd6d4fa75801adaf
+                  MATRIX: 509->1|627->31|655->33|732->84|757->89
+                  LINES: 19->1|22->1|24->3|28->7|28->7
+                  -- GENERATED --
+              */
+          
